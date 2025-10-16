b7741727ac10e84854ba5875f00612d47ee71d28
danielsj2-afk <danielsj2@hocking.edu>
2025-10-16 15:31:15 +0000

    Changed Numbers

--- tester.py
+++ tester.py
@@ -2,9 +2,10 @@
 def add_numbers(a, b):
     return a + b
 
+
 if __name__ == "__main__":
-    num1 = 5
-    num2 = 7
+    num1 = 10
+    num2 = 5
     result = add_numbers(num1, num2)
     print(f"{num1} + {num2} = {result}")
 
